e32984ccb7320e7b486fadba633ec8a814fae63b
HoKhua22 <khua.ho22@student.passerellesnumeriques.org>
2021-12-23 08:29:44 +0700

    implementing caculate

---
+++ components/Button.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text, Dimensions } from 'react-native';
+
+const screen = Dimensions.get('window');
+const buttonWidth = screen.width / 4;
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#fff',
+    fontSize: 25,
+  },
+  textSecondary: {
+    color: '#060606',
+  },
+  button: {
+    backgroundColor: '#333333',
+    flex: 1,
+    height: Math.floor(buttonWidth - 10),
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: Math.floor(buttonWidth),
+    margin: 5,
+  },
+  buttonDouble: {
+    width: screen.width / 2 - 10,
+    flex: 0,
+    alignItems: 'flex-end',
+    paddingRight: 40,
+  },
+  buttonLength: {
+    flex: 1,
+    // position: 'absolute',
+    // left: 100,
+    // top: -20,
+    justifyContent: 'flex-end',
+    height: screen.width / 2 - 10,
+    backgroundColor: '#FF6699',
+  },
+  buttonSecondary: {
+    backgroundColor: '#a6a6a6',
+  },
+  buttonAccent: {
+    backgroundColor: '#f09a36',
+  },
+});
+
+export default ({ onPress, text, size, theme }) => {
+  const buttonStyles = [styles.button];
+  const textStyles = [styles.text];
+
+  if (size === 'double') {
+    buttonStyles.push(styles.buttonDouble);
+  }
+
+  if (size == 'long') {
+    buttonStyles.push(styles.buttonLength);
+  }
+
+  if (theme === 'secondary') {
+    buttonStyles.push(styles.buttonSecondary);
+    textStyles.push(styles.textSecondary);
+  } else if (theme === 'accent') {
+    buttonStyles.push(styles.buttonAccent);
+  }
+
+  return (
+    <TouchableOpacity onPress={onPress} style={buttonStyles}>
+      <Text style={textStyles}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
---
+++ util/calculator.js
@@ -0,0 +1,89 @@
+export const initialState = {
+  showCaculator: '',
+  currentValue: '0',
+  operator: null,
+  previousValue: null,
+};
+
+export const handleNumber = (value, state) => {
+  if (state.currentValue === '0') {
+    return {
+      currentValue: `${value}`,
+      showCaculator: state.showCaculator.concat(`${value}`),
+    };
+  }
+  return {
+    currentValue: `${state.currentValue}${value}`,
+  };
+};
+
+export const handleEqual = (state) => {
+  const { currentValue, previousValue, operator, showCaculator } = state;
+
+  const current = parseFloat(currentValue);
+  const previous = parseFloat(previousValue);
+  const resetState = {
+    operator: null,
+    previousValue: null,
+  };
+
+  if (operator === '/') {
+    return {
+      currentValue: previous / current,
+      ...resetState,
+    };
+  }
+
+  if (operator === '*') {
+    return {
+      currentValue: previous * current,
+      ...resetState,
+    };
+  }
+
+  if (operator === '+') {
+    return {
+      currentValue: previous + current,
+      ...resetState,
+    };
+  }
+
+  if (operator === '-') {
+    return {
+      currentValue: previous - current,
+      ...resetState,
+    };
+  }
+
+  return state;
+};
+
+const calculator = (type, value, state) => {
+  switch (type) {
+    case 'number':
+      return handleNumber(value, state);
+    case 'operator':
+      return {
+        operator: value,
+        previousValue: state.currentValue,
+        currentValue: '0',
+        showCaculator: state.showCaculator.concat(value),
+      };
+    case 'equal':
+      return handleEqual(state);
+    case 'clear':
+      return initialState;
+    case 'posneg':
+      return {
+        currentValue: `${parseFloat(state.currentValue) * -1}`,
+      };
+    case 'percentage':
+      return {
+        currentValue: `${parseFloat(state.currentValue) * 0.01}`,
+      };
+    default:
+      return state;
+  }
+};
+
+export default calculator;
